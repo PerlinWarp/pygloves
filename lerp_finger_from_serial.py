06e0fa7a0449be11a15b330e339c10e4c5545d65
perlinwarp <perlinwarp@gmail.com>
2021-10-01 18:26:21 +0100

    Cleaning: Moving serial decodes into a module

--- lerp_finger_from_serial.py
+++ lerp_finger_from_serial.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 import bone
+import serial_utils as s
 
 # Use device manager to find the Arduino's serial port.
 COM_PORT = "COM9"
-RESET_SCALE = False
+RESET_SCALE = True
 LEGACY_DECODE = False # If false, will use alpha encodings
 
 q = multiprocessing.Queue()
@@ -33,42 +34,15 @@ def serial_worker(q):
 			# Read from serial
 			read = ser.readline()
 			if LEGACY_DECODE:
-				fingers = decode_legacy_serial(read)
+				fingers = s.decode_legacy_serial(read)
 			else:
-				fingers = decode_alpha_serial(read)
+				fingers = s.decode_alpha_serial(read)
 			# Add the decoded values to the queue
 			q.put(fingers)
 		except KeyboardInterrupt:
 			print("Quitting thread...")
 			ser.close()
 			quit()
-
-def decode_legacy_serial(s):
-	if s == b'':
-		print(s)
-		return [0,0,0,0,0]
-	else:
-		# Decode the byte string to list of ints
-		s = s.decode().rstrip().split('&')
-		# Get rid of all other data than fingers
-		s = s[0:5]
-		# Cast to ints
-		s = [int(f) for f in s]
-		return s
-
-def decode_alpha_serial(s):
-	if (s == b''):
-		return [0,0,0,0,0]
-	else:
-		# Decode from bytes
-		x = s.decode()
-		# Split between numbers and letters
-		x = re.split('(\d+)', x.rstrip())
-		# Pull the flexion data out, Get rid of all other data than fingers
-		fingers = [x[1], x[3], x[5], x[7], x[9]]
-		# Cast to ints
-		s = [int(f) for f in fingers]
-		return s
 
 def animate(i):
 	fingers = [0,0,0,0,0]
@@ -89,7 +63,7 @@ def animate(i):
 	ax.set_ylabel('Y [mm]')
 	ax.set_zlabel('Z [mm]')
 
-	open_pose = bone.right_pose
+	open_pose = bone.right_open_pose
 	closed_pose = bone.right_fist_pose
 
 	# Split up the hand
